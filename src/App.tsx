7a84cfbaaaa52df9c8793ac883d153e535d49d80
Chris Stephens <chris@Chriss-MacBook-Pro.local>
2024-01-03 21:04:40 +0100

    add: routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
-import { useState } from 'react'
-import './App.css'
-import LoginContext from './contexts/LoginContext'
+import { useState } from "react";
+import "./App.css";
+import LoginContext from "./contexts/LoginContext";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import DashBoard from "./components/DashBoard";
 
 function App() {
-  const [loginStatus, setLoginStatus] = useState<boolean>(true);
+    const [loginStatus, setLoginStatus] = useState<boolean>(true);
 
-  return (
-    <>
-      <LoginContext.Provider value={loginStatus}>
-
-    </LoginContext.Provider>
-    </>
-  )
+    return (
+        <>
+            <LoginContext.Provider value={loginStatus}>
+                <Router>
+                    <Routes>
+                        {!loginStatus && <Route path="/home" element={<Home />} />}
+                        {loginStatus && <Route path="/dashboard" element={<DashBoard />}/>}
+                    </Routes>
+                </Router>
+            </LoginContext.Provider>
+        </>
+    );
 }
 
-export default App
+export default App;
